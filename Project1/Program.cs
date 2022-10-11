9cbf18a903d8ce640c402063c8a158f32e6afc92
ElinaVallianos <115302861+ElinaVallianos@users.noreply.github.com>
2022-10-11 19:38:31 +0200

    input

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,16 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
 
-float a = 10000.1f;
-float b = 10000.01f;
+        float a = 10000.1f;
+        float b = 10000.01f;
 
-double c = 0.123451;
-b = (float)c;
-Console.WriteLine(c);
+        double c = 0.123451;
+        b = (float)c;
+        Console.WriteLine(c);
 
-byte b1 = 15;
-int i1 = 257;
+        byte b1 = 15;
+        int i1;
 
-b1 = (byte) i1;
-Console.WriteLine(b1);
+        i1 = ++b1;
+        Console.WriteLine(b1);
+        Console.WriteLine(i1);
 
+        int i2 = 4;
+        int i3 = 11;
+
+
+        Console.WriteLine(i3/i2);
+        Console.WriteLine(i3%i2);
+
+        Console.WriteLine("Enter your number: ");
+        string input = Console.ReadLine();
+        i3 = int.Parse(input);
+        Console.WriteLine($"You enter: {i3} ");
+        //Console.WriteLine(i3);
+    }
+}
