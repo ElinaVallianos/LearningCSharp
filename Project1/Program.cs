677c9c9de91d6ef9cac4e1d28799d756b767410f
ElinaVallianos <elina.vallianos@icloud.com>
2022-10-11 16:15:08 +0200

    ClassWork

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+float a = 10000.1f;
+float b = 10000.01f;
+
+double c = 0.123451;
+b = (float)c;
+Console.WriteLine(c);
+
+byte b1 = 15;
+int i1 = 257;
+
+b1 = (byte) i1;
+Console.WriteLine(b1);
+
