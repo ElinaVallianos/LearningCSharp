4682399e16104ef74ff80c497327fa7c061734e4
ElinaVallianos <115302861+ElinaVallianos@users.noreply.github.com>
2022-10-18 16:06:43 +0200

    lesson 3
    class work

---
+++ Lesson3.2/Program.cs
@@ -0,0 +1,33 @@
+﻿byte byteNumber = 13;
+byte bytenumber2 = 15;
+short shortNumber = 32124;
+int intNumber = 1235456;
+long LongNumber = 632156155;
+
+LongNumber = intNumber;
+
+//byteNumber = (byte)shortNumber;
+shortNumber = (short)intNumber;
+
+LongNumber = LongNumber + intNumber;
+
+LongNumber += intNumber;
+Console.WriteLine(byteNumber);
+
+string string1, string2;
+
+string1 = "My name ";
+string2 = "Peter";
+
+string1 += string2;
+
+Console.WriteLine(string1);
+
+Console.WriteLine(byteNumber > bytenumber2);
+Console.WriteLine(byteNumber + 2 >= bytenumber2);
+Console.WriteLine(byteNumber + 2 <= bytenumber2);
+Console.WriteLine(byteNumber + 2 == bytenumber2);
+Console.WriteLine(byteNumber + 2 != bytenumber2);
+
+
+
--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -1,2 +1,21 @@
-﻿
+﻿int number;
+
+bool isEven;
+
+
+Console.Write("Write your number: ");
+number = int.Parse(Console.ReadLine());
+
+if (number%2 != 0)
+{
+    isEven = false;
+    Console.WriteLine($"The number {number} is not even.");
+}
+else
+{
+    isEven = true;
+    Console.WriteLine($"The number {number} is even.");
+}
+
+
 
