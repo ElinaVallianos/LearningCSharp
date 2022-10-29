81153a927bc585eb800cd3fba67716350b58db99
ElinaVallianos <115302861+ElinaVallianos@users.noreply.github.com>
2022-10-29 18:05:59 +0200

    HomeWork4.Fix

--- HomeWork4.1/Program.cs
+++ HomeWork4.1/Program.cs
@@ -10,15 +10,90 @@ bool NUMBER = true;
 NUMBER = n2 < n1 && n1 < n3;
 Console.WriteLine(NUMBER);
 
-int nBig = n1 > n2 ? n1 : n2;
-nBig = nBig > n3 ? nBig : n3;
-int nSmall = n1 > n2 ? n2 : n1;
-nSmall = n3 > nSmall ? nSmall : n3;
-int nMedium = n1;
-nMedium = (nMedium == nBig || nMedium == nSmall) ? n2 : nMedium;
-nMedium = (nMedium == nSmall || nMedium == nBig) ? n3 : nMedium;
+int nBig;
+int nSmall;
+int nMedium;
+if (n1 == n2 || n1 == n3 || n2 == n3)
+{
+    if (n1 == n2 && n2 == n3)
+    {
+        Console.WriteLine($"{n1} = {n2} = {n3}");
+        nBig = n1;
+        nMedium = nBig;
+        nSmall = nBig;
+    }
+    else
+    {
+        if (n1 == n2)
+        {
+            if (n1 > n3)
+            {
+                Console.WriteLine($"{n3} < {n2} = {n1}");
+                nBig = n1;
+                nSmall = n3;
+                nMedium = nBig;
+            }
+            else
+            {
+                Console.WriteLine($"{n2} = {n1} < {n3}");
+                nBig = n3;
+                nMedium = nBig;
+                nSmall = n2;
+            }
+        }
+        else
+        {
+            if (n1 == n3)
+            {
+                if (n2 > n1)
+                {
+                    Console.WriteLine($"{n1} = {n3} < {n2}");
+                    nBig = n2;
+                    nMedium = nBig;
+                    nSmall = n1;
+                }
+                else
+                {
+                    Console.WriteLine($"{n2} < {n3} = {n1}");
+                    nBig = n1;
+                    nMedium = nBig;
+                    nSmall = n2;
+                }
+            }
+            else
+            {
+                if (n1 > n2)
+                {
+                    Console.WriteLine($"{n2} = {n3} < {n1}");
+                    nBig = n1;
+                    nMedium = nBig;
+                    nSmall = n2;
+                }
+                else
+                {
+                    Console.WriteLine($"{n1} < {n3} = {n2}");
+                    nBig = n2;
+                    nMedium = nBig;
+                    nSmall = n1;
+                }
+            }
+        }
+    }
+}
+else
+{
+    nBig = n1 > n2 ? n1 : n2;
+    nBig = nBig > n3 ? nBig : n3;
+    nSmall = n1 > n2 ? n2 : n1;
+    nSmall = n3 > nSmall ? nSmall : n3;
+    nMedium = n1;
+    nMedium = (nMedium == nBig || nMedium == nSmall) ? n2 : nMedium;
+    nMedium = (nMedium == nSmall || nMedium == nBig) ? n3 : nMedium;
 
-Console.WriteLine($"{nSmall} < {nMedium} < {nBig}");
+    Console.WriteLine($"{nSmall} < {nMedium} < {nBig}");
+}
+
+
 
 
 int count = nSmall;
